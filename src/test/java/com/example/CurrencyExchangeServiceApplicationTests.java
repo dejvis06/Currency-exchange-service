cad2d16b480039c90cb128457ebaf92f718c9883
dbeqiraj <dejvisbeqiraj06@gmail.com>
2022-02-17 10:40:50 +0100

    Saved currency exchange data in db;

--- src/test/java/com/example/CurrencyExchangeServiceApplicationTests.java
+++ src/test/java/com/example/CurrencyExchangeServiceApplicationTests.java
@@ -1,13 +1,29 @@
 package com.example;
 
+import com.example.entity.CurrencyExchange;
+import com.example.repository.CurrencyExchangeRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
 
 @SpringBootTest
 class CurrencyExchangeServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private CurrencyExchangeRepository currencyExchangeRepository;
+
+    @Test
+    void contextLoads() {
+
+        CurrencyExchange currencyExchange = new CurrencyExchange();
+        currencyExchange.setFrom("USD");
+        currencyExchange.setTo("INR");
+        currencyExchange.setConversionMultiple(new BigDecimal(65));
+        currencyExchange.setEnvironment("8000");
+
+        currencyExchangeRepository.save(currencyExchange);
+    }
 
 }
